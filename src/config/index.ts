e1a46b99b10ad0cd33b1c02dcbb9fb1c14476335
TommyGC11 <tommycontreras34@gmail.com>
2024-10-01 16:41:00 -0400

    feat: :sparkles: add minio config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 export const objectStorageConfig = {
-    bucket: process.env.MINIO_BUCKET as string,
-    url: process.env.MINIO_URL as string,
-    port: parseInt(`${process.env.MINIO_PORT}`),
-    ssl: process.env.MINIO_SSL == "true",
-    accessKey: process.env.MINIO_ACCESS_KEY as string,
-    secretKey: process.env.MINIO_SECRET_KEY as string
+    bucket: process.env.OBJECT_STORAGE_BUCKET as string,
+    url: process.env.OBJECT_STORAGE_URL as string,
+    port: parseInt(`${process.env.OBJECT_STORAGE_PORT}`),
+    ssl: process.env.OBJECT_STORAGE_SSL == "true",
+    accessKey: process.env.OBJECT_STORAGE_ACCESS_KEY as string,
+    secretKey: process.env.OBJECT_STORAGE_SECRET_KEY as string
 }
